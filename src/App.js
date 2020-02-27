9d269b944c6104f74e4be12581dfe322a4306fa7
banobepascal <banobepascal@gmail.com>
2020-02-27 13:55:22 +0300

    ch(added users)
    Finishes

--- src/App.js
+++ src/App.js
@@ -7,21 +7,76 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [
+          {
+            id: 1,
+            name: "Leanne Graham",
+            username: "Bret",
+            email: "Sincere@april.biz",
+          },
+          {
+            id: 2,
+            name: "Ervin Howell",
+            username: "Antonette",
+            email: "Shanna@melissa.tv",
+          },
+          {
+            id: 3,
+            name: "Clementine Bauch",
+            username: "Samantha",
+            email: "Nathan@yesenia.net",
+          },
+          {
+            id: 4,
+            name: "Patricia Lebsack",
+            username: "Karianne",
+            email: "Julianne.OConner@kory.org",
+          },
+          {
+            id: 5,
+            name: "Chelsey Dietrich",
+            username: "Kamren",
+            email: "Lucio_Hettinger@annie.ca",
+          },
+          {
+            id: 6,
+            name: "Mrs. Dennis Schulist",
+            username: "Leopoldo_Corkery",
+            email: "Karley_Dach@jasper.info",
+          },
+          {
+            id: 7,
+            name: "Kurtis Weissnat",
+            username: "Elwyn.Skiles",
+            email: "Telly.Hoeger@billy.biz",
+          },
+          {
+            id: 8,
+            name: "Nicholas Runolfsdottir V",
+            username: "Maxime_Nienow",
+            email: "Sherwood@rosamond.me",
+          },
+          {
+            id: 9,
+            name: "Glenna Reichert",
+            username: "Delphine",
+            email: "Chaim_McDermott@dana.io",
+          },
+          {
+            id: 10,
+            name: "Clementina DuBuque",
+            username: "Moriah.Stanton",
+            email: "Rey.Padberg@karina.biz",
+          }
+        ]
     };
   }
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(res => res.json())
-      .then(users => this.setState({ monsters: users }));
-  }
 
   render() {
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}>   
-        </CardList>
+        <CardList monsters={this.state.monsters}></CardList>
       </div>
     );
   }
