3a91bfc40d783f8a9d1236b05858b776525cb685
banobepascal <banobepascal@gmail.com>
2020-02-27 16:12:44 +0300

    ft(adding search-box component to App)
    - added more styles to App.css
    
    Finishes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-import  CardList  from "./components/card-list/card-list.component";
 import "./App.css";
+
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
@@ -8,75 +10,72 @@ class App extends Component {
 
     this.state = {
       monsters: [
-          {
-            id: 1,
-            name: "Leanne Graham",
-            username: "Bret",
-            email: "Sincere@april.biz",
-          },
-          {
-            id: 2,
-            name: "Ervin Howell",
-            username: "Antonette",
-            email: "Shanna@melissa.tv",
-          },
-          {
-            id: 3,
-            name: "Clementine Bauch",
-            username: "Samantha",
-            email: "Nathan@yesenia.net",
-          },
-          {
-            id: 4,
-            name: "Patricia Lebsack",
-            username: "Karianne",
-            email: "Julianne.OConner@kory.org",
-          },
-          {
-            id: 5,
-            name: "Chelsey Dietrich",
-            username: "Kamren",
-            email: "Lucio_Hettinger@annie.ca",
-          },
-          {
-            id: 6,
-            name: "Mrs. Dennis Schulist",
-            username: "Leopoldo_Corkery",
-            email: "Karley_Dach@jasper.info",
-          },
-          {
-            id: 7,
-            name: "Kurtis Weissnat",
-            username: "Elwyn.Skiles",
-            email: "Telly.Hoeger@billy.biz",
-          },
-          {
-            id: 8,
-            name: "Nicholas Runolfsdottir V",
-            username: "Maxime_Nienow",
-            email: "Sherwood@rosamond.me",
-          },
-          {
-            id: 9,
-            name: "Glenna Reichert",
-            username: "Delphine",
-            email: "Chaim_McDermott@dana.io",
-          },
-          {
-            id: 10,
-            name: "Clementina DuBuque",
-            username: "Moriah.Stanton",
-            email: "Rey.Padberg@karina.biz",
-          }
-        ]
+        {
+          id: 1,
+          name: "Leanne Graham",
+          username: "Bret",
+          email: "Sincere@april.biz"
+        },
+        {
+          id: 2,
+          name: "Ervin Howell",
+          username: "Antonette",
+          email: "Shanna@melissa.tv"
+        },
+        {
+          id: 3,
+          name: "Clementine Bauch",
+          username: "Samantha",
+          email: "Nathan@yesenia.net"
+        },
+        {
+          id: 4,
+          name: "Patricia Lebsack",
+          username: "Karianne",
+          email: "Julianne.OConner@kory.org"
+        },
+        {
+          id: 5,
+          name: "Chelsey Dietrich",
+          username: "Kamren",
+          email: "Lucio_Hettinger@annie.ca"
+        },
+        {
+          id: 6,
+          name: "Mrs. Dennis Schulist",
+          username: "Leopoldo_Corkery",
+          email: "Karley_Dach@jasper.info"
+        },
+        {
+          id: 7,
+          name: "Kurtis Weissnat",
+          username: "Elwyn.Skiles",
+          email: "Telly.Hoeger@billy.biz"
+        },
+        {
+          id: 8,
+          name: "Nicholas Runolfsdottir V",
+          username: "Maxime_Nienow",
+          email: "Sherwood@rosamond.me"
+        }
+      ],
+      searchField: ""
     };
   }
 
-
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}></CardList>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
   }
