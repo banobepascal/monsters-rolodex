b9ccf80c839a1b8112afd18dfbbaa41decaffe7c
banobepascal <banobepascal@gmail.com>
2020-02-29 00:01:16 +0300

    Updates

---
+++ precache-manifest.222db6bd2dfd8aa785cbd798315fabca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d83a3491c5ee042015499d481e147b4a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c62a29cd76fb7a1d2098",
+    "url": "/monsters-rolodex/static/css/main.185c3464.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c62a29cd76fb7a1d2098",
+    "url": "/monsters-rolodex/static/js/main.d0c23426.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
